7ab5c3ba3fe33685b90db768d614c3c8a749bc0f
zmar0519 <zachary.p.martin7@gmail.com>
2021-08-24 11:47:46 -0500

    change about to request

--- main_app/views.py
+++ main_app/views.py
@@ -6,4 +6,4 @@ def home(request):
   return HttpResponse('<h1>Hello world</h1>')
 
 def about(request):
-  return HttpResponse('<h1>About this fine todo app</h1>')
+  return render(request, 'about.html')
