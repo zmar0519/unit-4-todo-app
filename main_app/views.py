569f2b7a4eda55dda7ae8befc58cd07e39c70420
zmar0519 <zachary.p.martin7@gmail.com>
2021-08-24 11:39:25 -0500

    stub up about page

--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Hello world</h1>')
+
+def about(request):
+  return HttpResponse('<h1>About this fine todo app</h1>')
