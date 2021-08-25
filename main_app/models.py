53d87b1181cb1be68b01aadd2413456b65653995
zmar0519 <zachary.p.martin7@gmail.com>
2021-08-25 14:29:28 -0500

    update index html

--- main_app/models.py
+++ main_app/models.py
@@ -6,3 +6,6 @@ class Task(models.Model):
   dueDate = models.DateField()
   description = models.CharField(max_length=500)
   isComplete = models.BooleanField(default=False)
+
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -8,6 +8,7 @@ def about(request):
   return render(request, 'about.html')
 
 def tasks_index(request):
+  tasks = Task.objects.all()
   return render(request, 'tasks/index.html', { 'tasks': tasks })
 
 class Task:
