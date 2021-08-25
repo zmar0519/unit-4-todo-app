cd3cafdd215b96eb869fce961d1c7e6c36cb37dd
zmar0519 <zachary.p.martin7@gmail.com>
2021-08-25 11:40:45 -0500

    build out views functions

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('tasks/', views.tasks_index, name='tasks_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,18 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def tasks_index(request):
+  return render(request, 'tasks/index.html', { 'tasks': tasks })
+
+class Task:
+  def __init__(self, name, dueDate, description, isComplete):
+      self.name = name
+      self.dueDate = dueDate
+      self.description = description
+      self.isComplete = isComplete
+
+tasks = [
+  Task('Build todo app', '9/27/2021', 'Python Project', False),
+  Task('Test', '9/28/2021', 'Test', False),
+]
