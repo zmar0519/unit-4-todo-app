e16ce79419dd8977960a8f6afd249ca2b9a8e428
zmar0519 <zachary.p.martin7@gmail.com>
2021-08-25 13:35:57 -0500

    create task model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+  name = models.CharField(max_length=100)
+  dueDate = models.DateField()
+  description = models.CharField(max_length=500)
+  isComplete = models.BooleanField(default=False)
