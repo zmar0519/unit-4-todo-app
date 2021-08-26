42074ad859b8813da406d043c637af2a7e66ab74
zmar0519 <zachary.p.martin7@gmail.com>
2021-08-26 16:28:44 -0500

    fix bug

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('tasks/', views.tasks_index, name='tasks_index'),
-    path('tasks/<int:task_id>/', views.tasks_detail, name='tasks_detail'),
-    path('tasks/create/', views.TaskCreate.as_view(), name='tasks_create')
+    path('tasks/<int:task_id>/', views.tasks_detail, 
+    name='tasks_detail'),
+    path('tasks/create/', views.TaskCreate.as_view(), 
+    name='tasks_create')
 ]
