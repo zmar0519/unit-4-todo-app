d05570a89199e12c85fe851215a16492ef1e35f2
zmar0519 <zachary.p.martin7@gmail.com>
2021-08-26 22:54:35 -0500

    start implementing auth

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('tasks/', views.tasks_index, name='tasks_index'),
     path('tasks/<int:task_id>/', views.tasks_detail, 
