77f169457dd753c46ffaebbd3347a791e170a63b
zmar0519 <zachary.p.martin7@gmail.com>
2021-08-27 11:50:19 -0500

    change to django-on-heroku

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -128,5 +128,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import django_heroku
-django_heroku.settings(locals())
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
