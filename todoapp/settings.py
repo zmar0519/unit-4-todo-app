b0fe15e195a536ebaee9bc437ad44a7ede7d061b
zmar0519 <zachary.p.martin7@gmail.com>
2021-08-30 13:02:56 -0500

    fix debug error

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -28,6 +28,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') == 'True'
 
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
 ALLOWED_HOSTS = []
 
 
